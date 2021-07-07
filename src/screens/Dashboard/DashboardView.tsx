c32ab0e27befae104c92c272660add30ba7852da
Alexandre Cechinel <alexandrecechinel@Alexandres-Air.box>
2021-07-06 21:07:38 -0300

    [dev] - Add user photo, name and using expo icons

--- src/screens/Dashboard/DashboardView.tsx
+++ src/screens/Dashboard/DashboardView.tsx
@@ -1,10 +1,36 @@
 import React from "react";
-import { Container, Title } from './DashboardViewStyled';
+import {
+  Container,
+  Header,
+  UserContainer,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from "./DashboardViewStyled";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserContainer>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/1832092?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olar, </UserGreeting>
+              <UserName>Alexandre</UserName>
+            </User>
+          </UserInfo>
+
+		  <Icon name="power" />
+        </UserContainer>
+      </Header>
     </Container>
   );
 }
